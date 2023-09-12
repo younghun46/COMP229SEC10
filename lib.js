040b88407845f9a90cebdfb6b0a475fcbd435069
younghun46 <dudgns5379@gmail.com>
2023-09-12 10:46:58 -0400

    first commit

---
+++ lib.js
@@ -0,0 +1,8 @@
+export function halfOf(x){
+    return x / 2;
+}
+
+
+export function multiply(x,y){
+    return x * y;
+}
---
+++ main.js
@@ -0,0 +1,7 @@
+// import { flag, touch } from './validator.js';
+// console.log(flag);
+// touch();
+// console.log(flag);
+import {halfOf} from './lib.js';
+console.log(halfOf(84));
+console.log(multiply(21, 2));
